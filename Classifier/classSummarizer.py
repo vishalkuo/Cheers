9ba696b88c059d7b93ed1124a5ef8fa628f40407
Vishal Kuo <vfkuo@uwaterloo.ca>
2015-10-05 18:53:23 -0400

    Split

--- Classifier/classSummarizer.py
+++ Classifier/classSummarizer.py
@@ -10,3 +10,24 @@ def std_dev(input_list):
     avg = mean(input_list)
     variance = sum([pow(x - avg, 2) for x in input_list]) / float(len(input_list) - 1)
     return math.sqrt(variance)
+
+def extractClasses(dataset, class_index):
+    separated = {}
+    for i in range(len(dataset)):
+        row = dataset[i]
+        if (row[class_index] not in separated):
+            separated[row[class_index]] = []
+        separated[row[class_index]].append(row)
+    return separated
+
+def summarize(list, class_index):
+    resultSet = [(mean(x), std_dev(x)) for x in zip(*list)]
+    del resultSet[class_index]
+    return resultSet
+
+def summarizeClasses(input_list, class_index):
+    separatedSet = extractClasses(input_list, class_index)
+    summarySet = {}
+    for class_value, instances in separatedSet.iteritems():
+        summarySet[class_value] = summarize(instances, class_index)
+    return summarySet
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import Utils.reader as reader
+import Utils.reader as reader, Classifier.classSummarizer as summarizer
 
 filename = 'Assets/wine.csv'
 totalSet = reader.loadFile(filename)
 
 trainingSet, testingSet = reader.getTrainAndTestSet(totalSet, 0.67)
+classSummaries = summarizer.summarizeClasses(totalSet, 0)
+print classSummaries
